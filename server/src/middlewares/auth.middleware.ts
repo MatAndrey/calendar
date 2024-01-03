937ee675dfb46f9a49774e95351af9a243ee709c
Andrey Matyushin <andrey.matyushin05@gmail.com>
2024-01-03 12:09:08 +0300

    fixes for cyclic

--- server/src/middlewares/auth.middleware.ts
+++ server/src/middlewares/auth.middleware.ts
@@ -11,18 +11,25 @@ interface JWT {
 export async function authMiddleware(req: Request, res: Response, next: NextFunction) {
     const token = req.headers.authorization?.split(" ")[1];
     if (token) {
-        const decoded = jwt.verify(token, SECRET_KEY) as JWT;
-        const name = decoded.name;
-        const password = decoded.password;
-        if (name && password) {
-            const user = await usersCollection.findOne({name, password })
-            if (user) {
-                req.user = {
-                    name,
-                    password,
-                };
+        jwt.verify(token, SECRET_KEY, async (err, decoded) => {
+            if (err) {
+                res.status(401).json({ message: "Invalid token" });
             }
-        }
+            if (decoded) {
+                decoded = decoded as JWT;
+                const name = decoded.name;
+                const password = decoded.password;
+                if (name && password) {
+                    const user = await usersCollection.findOne({ name, password });
+                    if (user) {
+                        req.user = {
+                            name,
+                            password,
+                        };
+                    }
+                }
+            }
+        });
     }
     next();
 }
